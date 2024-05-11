8059b341f9e393f49e56a4c2422cf735ecb046ac
jrx <jrepnak@hashicorp.com>
2024-05-11 14:02:20 +0200

    Set 1, Challenge 2

--- set1.go
+++ set1.go
@@ -3,6 +3,7 @@ package cryptopals
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"log"
 )
 
@@ -13,4 +14,29 @@ func HexToBase64(hs string) (string, error) {
 	}
 	log.Printf("%s", value)
 	return base64.StdEncoding.EncodeToString(value), nil
+}
+
+func FixedXOR(s1, s2 string) (string, error) {
+	h1, err := hex.DecodeString(s1)
+	if err != nil {
+		return "", err
+	}
+	log.Printf("%s", string(h1))
+
+	h2, err := hex.DecodeString(s2)
+	if err != nil {
+		return "", err
+	}
+	log.Printf("%s", string(h2))
+
+	if len(h1) != len(h2) {
+		return "", errors.New("invalid length")
+	}
+
+	res := make([]byte, len(h1))
+	for i := range h1 {
+		res[i] = h1[i] ^ h2[i]
+	}
+	log.Printf("%s", string(res))
+	return hex.EncodeToString(res), nil
 }
--- set1_test.go
+++ set1_test.go
@@ -14,3 +14,14 @@ func TestHexToBase64(t *testing.T) {
 		t.Errorf("Expected %s, got %s", base64String, res)
 	}
 }
+
+func TestFixedXOR(t *testing.T) {
+	expected := "746865206b696420646f6e277420706c6179"
+	res, err := FixedXOR("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+	if res != expected {
+		t.Errorf("Expected %s, got %s", expected, res)
+	}
+}
